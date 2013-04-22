5eaf9ed5f049471adfd534cc71e6c05f6d435abf
Shiloh Chi <shilohchi@gmail.com>
2013-04-22 14:11:09 +0800

    添加对ConfigSingleton.cpp的测试

---
+++ test/test-ConfigSingleton.cpp
@@ -0,0 +1,34 @@
+#include "ConfigSingleton.h"
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	multimap<string, string>* config = ConfigSingleton::getConfig();
+	multimap<string, string>::iterator it = config->find("network-interface");
+	cout << it->second << endl;
+
+	it = config->find("database-host");
+	cout << it->second << endl;
+	
+	it = config->find("database-user");
+	cout << it->second << endl;
+	
+	it = config->find("database-password");
+	cout << it->second << endl;
+
+	for (it = config->lower_bound("low-level-filter"); 
+			it != config->upper_bound("low-level-filter");
+			it++) {
+		cout << "low level filter: " << it->second << endl;
+	}
+
+
+	for (it = config->lower_bound("application-layer-filter"); 
+			it != config->upper_bound("application-layer-filter");
+			it++) {
+		cout << "application layer filter: " << it->second << endl;
+	}
+}
